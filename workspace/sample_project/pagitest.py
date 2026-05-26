208baa0e04ad5f0f6cdcd5deba5bdd293bfcc92f
Igor Peterson <igor.petersson@gmail.com>
2026-05-26 15:01:34 +0200

    Fix calculator bug and add pagitest file

---
+++ workspace/sample_project/pagitest.py
@@ -0,0 +1,215 @@
+# =====================================================================
+# PAGINATION TEST FILE
+# This file is intentionally nonsensical and non-runnable.
+# Purpose: generate >4000 characters of malformed Python-like content.
+# =====================================================================
+
+def completely_broken_function(:
+    this is not valid python
+    and neither is this line
+    for ??? in ???:
+        print("undefined universe")
+
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+************************************************************************
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+BROKEN_SECTION_0001 = "alpha-beta-gamma-delta-epsilon-zeta-eta-theta"
+BROKEN_SECTION_0002 = "lambda-sigma-omega-nebula-fragment-overflow"
+BROKEN_SECTION_0003 = "pagination-pagination-pagination-pagination"
+BROKEN_SECTION_0004 = "this-file-should-never-be-executed-under-any-circumstances"
+
+if maybe perhaps unless == ???:
+    explode.everything(now)
+
+class UndefinedReality(
+    def __init__(self):
+        self.dimension = NULLVOIDMATRIX
+        self.gravity = "sideways"
+        self.language = [{{{{{{{{{{{{{{{{[[[[[[[]]]]]]]}}}}}}}}}}}}}}}}
+
+    def recurse_forever(self):
+        return self.recurse_forever_forever_forever_forever()
+
+############ RANDOM PAYLOAD START #####################################
+
+lorem_fragment_0001 = "Lorem ipsum dolor sit amet but corrupted"
+lorem_fragment_0002 = "Consectetur adipiscing elit #### !!!! ????"
+lorem_fragment_0003 = "Vestibulum overengineered pagination simulator"
+lorem_fragment_0004 = "1234567890abcdefghijklmnopqrstuvwxyz"
+lorem_fragment_0005 = "!@#$%^&*()_+-=[]{}|;':,./<>?"
+lorem_fragment_0006 = "THIS IS NOT REAL PYTHON CODE"
+lorem_fragment_0007 = "The quick brown fox jumps over invalid syntax"
+lorem_fragment_0008 = "Chunking chunking chunking chunking chunking"
+lorem_fragment_0009 = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
+lorem_fragment_0010 = "BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB"
+
+[
+[
+[
+[
+[
+[
+[
+[
+[
+[
+THIS
+IS
+NOT
+A
+LIST
+ANYMORE
+]
+]
+]
+]
+]
+]
+]
+]
+]
+]
+
+malformed_dictionary = {
+    "key1": "value1",
+    "key2": "value2",
+    "key3": ::::::::::::::::
+    "key4": "value4"
+    "missing_comma": true false maybe null undefined
+}
+
+#######################################################################
+# REPETITIVE NONSENSE BLOCKS
+#######################################################################
+
+BLOCK_A_0001 = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+BLOCK_A_0002 = "YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY"
+BLOCK_A_0003 = "ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ"
+BLOCK_A_0004 = "000000000000000000000000000000000000000000000000000"
+BLOCK_A_0005 = "111111111111111111111111111111111111111111111111111"
+BLOCK_A_0006 = "222222222222222222222222222222222222222222222222222"
+BLOCK_A_0007 = "333333333333333333333333333333333333333333333333333"
+BLOCK_A_0008 = "444444444444444444444444444444444444444444444444444"
+BLOCK_A_0009 = "555555555555555555555555555555555555555555555555555"
+BLOCK_A_0010 = "666666666666666666666666666666666666666666666666666"
+
+while impossible_condition === TRUEISH:
+    continue break explode vanish implode
+
+#######################################################################
+# FAKE CONFIGURATION DATA
+#######################################################################
+
+config = {
+    "server": "void://null.endpoint.infinity",
+    "port": -999999999,
+    "auth": "¯\_(ツ)_/¯",
+    "retry": "maybe",
+    "timeout": "eventually",
+    "cache": "enabled-but-disabled",
+    "pagination": {
+        "page_size": "banana",
+        "max_pages": "infinite+1",
+        "cursor": "<<<<>>>>><<<<>>>>"
+    }
+}
+
+#######################################################################
+# RANDOM MULTILINE CHAOS
+#######################################################################
+
+TEXT_BLOCK = """
+This is a fake multiline string designed to increase file size.
+It contains random sentences, malformed structures, and pointless text.
+Nothing in this document should ever be interpreted as executable code.
+The pagination system should split this content across multiple chunks.
+Repeating patterns improves visibility during pagination testing.
+ABCDEFGHIJKLMNOPQRSTUVWXYZ
+abcdefghijklmnopqrstuvwxyz
+0123456789012345678901234567890123456789
+
+The rain in hexadecimal falls mainly on the buffer overflow.
+Recursive pancakes stack beyond observable memory limits.
+Null pointers dream in electric brackets during segmentation faults.
+This line intentionally left syntactically incompatible.
+This line intentionally left syntactically incompatible.
+This line intentionally left syntactically incompatible.
+This line intentionally left syntactically incompatible.
+This line intentionally left syntactically incompatible.
+"""
+
+#######################################################################
+# EVEN MORE NONSENSE
+#######################################################################
+
+for item in universe.where(reality == broken:
+    print(item.item.item.item.item.item.item
+
+syntax_error_payload = [
+    "one",
+    "two",
+    "three",
+    "four",
+    "five"
+    "missing comma here"
+    "another impossible structure"
+]
+
+<xml>
+    <this>
+        <is>
+            <not>
+                python
+            </not>
+        </is>
+    </this>
+</xml>
+
+SELECT * FROM imaginary_table WHERE dragons = TRUE;
+DROP TABLE pagination_testing_maybe;
+COMMIT ROLLBACK ABORT IGNORE PANIC;
+
+#######################################################################
+# LENGTH EXPANSION AREA
+#######################################################################
+
+REPEATER_0001 = "pagination-test-pagination-test-pagination-test"
+REPEATER_0002 = "pagination-test-pagination-test-pagination-test"
+REPEATER_0003 = "pagination-test-pagination-test-pagination-test"
+REPEATER_0004 = "pagination-test-pagination-test-pagination-test"
+REPEATER_0005 = "pagination-test-pagination-test-pagination-test"
+REPEATER_0006 = "pagination-test-pagination-test-pagination-test"
+REPEATER_0007 = "pagination-test-pagination-test-pagination-test"
+REPEATER_0008 = "pagination-test-pagination-test-pagination-test"
+REPEATER_0009 = "pagination-test-pagination-test-pagination-test"
+REPEATER_0010 = "pagination-test-pagination-test-pagination-test"
+REPEATER_0011 = "pagination-test-pagination-test-pagination-test"
+REPEATER_0012 = "pagination-test-pagination-test-pagination-test"
+REPEATER_0013 = "pagination-test-pagination-test-pagination-test"
+REPEATER_0014 = "pagination-test-pagination-test-pagination-test"
+REPEATER_0015 = "pagination-test-pagination-test-pagination-test"
+REPEATER_0016 = "pagination-test-pagination-test-pagination-test"
+REPEATER_0017 = "pagination-test-pagination-test-pagination-test"
+REPEATER_0018 = "pagination-test-pagination-test-pagination-test"
+REPEATER_0019 = "pagination-test-pagination-test-pagination-test"
+REPEATER_0020 = "pagination-test-pagination-test-pagination-test"
+
+#######################################################################
+# FINAL BROKEN FOOTER
+#######################################################################
+
+END_OF_FILE_BUT_NOT_REALLY = >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+def last_error():
+    raise ThisShouldNeverCompile["catastrophic"]
+
+))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))
+(((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((
+
+THE_END_MAYBE = "EOF???"
